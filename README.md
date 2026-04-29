d8844188f5f2435b110b3bab46e2f66e443f0155
Ankita01git <ankitabaital@gmail.com>
2026-04-29 20:07:05 +0530

    Add language filter feature

---
+++ README.md
@@ -0,0 +1,2 @@
+# GitHub-profile-analyzer
+An profile analyzer  that take username and shows their stats and top respo
--- github_analyzer.py
+++ github_analyzer.py
@@ -3,7 +3,6 @@ import requests
 search= input("Enter your Github username:")
 
 response = requests.get(f"https://api.github.com/users/{search}")
-
 data = response.json()
 
 response1 = requests.get(f"https://api.github.com/users/{search}/repos")
@@ -12,6 +11,10 @@ data1 = response1.json()
 print(f"Name: {data['name']}, Bio : {data['bio']},Followers:{data['followers']}")
 
 output = sorted(data1, key=lambda x :x["stargazers_count"], reverse=True)
+lang_filter = input("Filter by language? (press Enter to skip):")
+
+if lang_filter:
+    output=[n for n in output if lang_filter in n['language'].lower()]
 
 for result in output[:5] :
     print(f"Name : {result['name']}, Language : {result.get('language', 'Unknown')}, star: {result['stargazers_count']}")
