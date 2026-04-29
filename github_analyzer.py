3723c4607354cbbefcde7e3c61050d8dcac6fd0c
Ankita01git <ankitabaital@gmail.com>
2026-04-29 20:30:07 +0530

    Handled null language and added chart by star

--- github_analyzer.py
+++ github_analyzer.py
@@ -1,4 +1,5 @@
 import requests
+import matplotlib.pyplot as plt
 
 search= input("Enter your Github username:")
 
@@ -14,7 +15,19 @@ output = sorted(data1, key=lambda x :x["stargazers_count"], reverse=True)
 lang_filter = input("Filter by language? (press Enter to skip):")
 
 if lang_filter:
-    output=[n for n in output if lang_filter in n['language'].lower()]
+     output = [n for n in output if lang_filter.lower() in (n['language'] or "").lower()]
 
-for result in output[:5] :
+# printing top 5 respo by star
+top_repos = output[:5]
+names = [result['name'] for result in top_repos]
+stars = [result['stargazers_count'] for result in top_repos]
+for result in top_repos :
     print(f"Name : {result['name']}, Language : {result.get('language', 'Unknown')}, star: {result['stargazers_count']}")
+
+# printing a chart of rating by star
+plt.bar(names, stars)
+plt.title("Top Repos by Stars")
+plt.xlabel("Repository")
+plt.ylabel("Stars")
+plt.tight_layout()
+plt.show()
